f23e01fc07d5e44505662fe06ab84a92d8a3b51c
yeotmani <ysn.outmani@gmail.com>
2021-10-26 23:20:46 +0200

    style

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
-      <View>
-        <TextInput placeholder="Course goals" style={{borderBottomColor:'black', borderWidth:1}}/>
-        <Button title="add"/>
-    </View>
-    <View>
-    </View>
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems:"center" }}>
+        <TextInput
+          placeholder="Course goals"
+          style={{ 
+           width: "80%" , borderBottomColor: "black", borderWidth: 1 }}
+        />
+        <Button title="add" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
